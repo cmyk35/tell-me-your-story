afa08ab51bb90ab5f4acda2362a9e29d48b10b87
escobra7 <carlogoerke@gmail.com>
2026-05-04 00:21:31 +0200

    chore: enhance logging configuration for Gunicornand update requirements

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from app.app import create_app
+import logging
 import os
+
+from app.app import create_app
 
 app = create_app()
 
@@ -7,4 +9,8 @@ if __name__ == '__main__':
   host = os.environ.get('HOST', os.environ.get('FLASK_RUN_HOST', '127.0.0.1'))
   port = int(os.environ.get('PORT', os.environ.get('FLASK_RUN_PORT', 5001)))
   app.run(host=host, port=port)
+elif "GUNICORN_CMD_ARGS" in os.environ:
+  gunicorn_logger = logging.getLogger("gunicorn.error")
+  app.logger.handlers = gunicorn_logger.handlers
+  app.logger.setLevel(gunicorn_logger.level)
   
